f4b580f5f627194558e65b8a5defdcc3ec41622a
John Murphy <john.murphy@datamars.com>
2021-02-13 17:54:56 +1300

    Add async.

--- jfm/src/app/app.component.ts
+++ jfm/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { timer } from 'rxjs';
+//import { setInterval } from 'timers';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'jfm';
+  title = 'jfm. ';
+  counter:number = 0;
+
+  ngOnInit() {
+    // const source$ = timer(1000,1000);
+    // source$.subscribe( this.onTick );
+
+  //   setInterval(() => {
+  //     this.title = this.title + '.';
+  //  }, 1000);
+
+    setInterval(() => this.onTick(this), 2000);
+  }
+
+  delay(ms: number) {
+    return new Promise( resolve => setTimeout(resolve, ms) );
+  }
+  
+  async onTick(a:AppComponent) {
+    console.log('tick');
+    a.counter++;
+    a.title += a.counter + ', ';
+
+    await this.delay(1000);
+    a.title += 'tock, ';
+  }
 }
